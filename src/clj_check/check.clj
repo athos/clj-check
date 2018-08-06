dde919d1b10af4ee18944a212d914654302c5838
Shogo Ohta <athos0220@gmail.com>
2018-08-06 15:14:07 +0900

    Check namespaces on classpath

--- src/clj_check/check.clj
+++ src/clj_check/check.clj
@@ -1,6 +1,27 @@
-(ns clj-check.check)
+(ns clj-check.check
+  (:require [bultitude.core :as b]
+            [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn check [source-paths]
+  (let [source-files (->> (or (seq source-paths) ["src"])
+                          (map io/file))
+        nses (b/namespaces-on-classpath :classpath source-files
+                                        :ignore-unreadable? false)]
+    (let [failures (atom 0)]
+      (doseq [ns nses]
+        (let [ns-file (-> (str ns)
+                          (.replace \- \_)
+                          (.replace \. \/))]
+          (binding [*out* *err*]
+            (println "Compiling namespace" ns))
+          (try
+            (binding [*warn-on-reflection* true]
+              (load ns-file))
+            (catch ExceptionInInitializerError e
+              (swap! failures inc)
+              (.printStackTrace e)))))
+      (if-not (zero? @failures)
+        (System/exit @failures)))))
+
+(defn -main [& source-paths]
+  (check source-paths))
