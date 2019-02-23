49efaf4bfda15519a8492bd583ac1c044b27216f
Derek Passen <dpassen1@gmail.com>
2019-02-23 17:52:21 -0600

    Rewrite without using an atom

--- src/clj_check/check.clj
+++ src/clj_check/check.clj
@@ -6,22 +6,27 @@
 
 (defn- file-for [ns] (-> ns name (str/replace \- \_) (str/replace \. \/)))
 
-(defn check [source-paths]
-  (let [nses (bultitude/namespaces-on-classpath
-              :classpath (map io/file source-paths)
-              :ignore-unreadable? false)]
-    (let [failures (atom 0)]
-      (doseq [ns nses]
-        (binding [*out* *err*]
-          (println "Compiling namespace" ns))
-        (try
-          (binding [*warn-on-reflection* true]
-            (load (file-for ns)))
-          (catch ExceptionInInitializerError e
-            (swap! failures inc)
-            (.printStackTrace e))))
-      (when-not (zero? @failures)
-        (System/exit @failures)))))
+(defn- reflection-error
+  [ns]
+  (binding [*out* *err*]
+    (println "Compiling namespace" ns))
+  (try
+    (binding [*warn-on-reflection* true]
+      (load (file-for ns)))
+    (catch ExceptionInInitializerError e
+      (doto e .printStackTrace))))
+
+(defn check
+  [source-paths]
+  (let [namespaces (bultitude/namespaces-on-classpath
+                    :classpath (map io/file source-paths)
+                    :ignore-unreadable? false)
+        failures   (count
+                    (sequence
+                     (comp (map reflection-error) (remove nil?))
+                     namespaces))]
+    (when-not (zero? failures)
+      (System/exit failures))))
 
 (defn -main [& source-paths]
   (check (or (seq source-paths) ["src"])))
