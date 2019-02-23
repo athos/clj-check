633b22421aeb698f8fb5187a86cd9575d84b6c82
Derek Passen <dpassen1@gmail.com>
2019-02-23 16:49:04 -0600

    Make use of clojure.string

--- src/clj_check/check.clj
+++ src/clj_check/check.clj
@@ -1,7 +1,10 @@
 (ns clj-check.check
   (:require
    [bultitude.core :as bultitude]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+(defn- file-for [ns] (-> ns name (str/replace \- \_) (str/replace \. \/)))
 
 (defn check [source-paths]
   (let [source-files (->> (or (seq source-paths) ["src"])
@@ -11,17 +14,14 @@
                       :ignore-unreadable? false)]
     (let [failures (atom 0)]
       (doseq [ns nses]
-        (let [ns-file (-> (str ns)
-                          (.replace \- \_)
-                          (.replace \. \/))]
-          (binding [*out* *err*]
-            (println "Compiling namespace" ns))
-          (try
-            (binding [*warn-on-reflection* true]
-              (load ns-file))
-            (catch ExceptionInInitializerError e
-              (swap! failures inc)
-              (.printStackTrace e)))))
+        (binding [*out* *err*]
+          (println "Compiling namespace" ns))
+        (try
+          (binding [*warn-on-reflection* true]
+            (load (file-for ns)))
+          (catch ExceptionInInitializerError e
+            (swap! failures inc)
+            (.printStackTrace e))))
       (if-not (zero? @failures)
         (System/exit @failures)))))
 
